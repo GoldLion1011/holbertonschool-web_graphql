4dad4cd5186be774a3dd8f21157278b6366af7f4
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-07-12 11:48:25 -0500

    tasks 2,3

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema'); // Assuming the schema.js file is in the same directory
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
   schema: schema, // Include the schema in the options object
-  graphiql: true, // Set graphiql to true if you want to enable the GraphiQL interface
+  graphiql: true // Set graphiql to true if you want to enable the GraphiQL interface
 }));
 
 app.listen(4000, () => {
