585a3291dc4af93075349e87d26537442c9d38b9
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-07-17 14:05:35 -0500

    unhide meeee

---
+++ GraphQL_API/client/src/components/TaskDetails.js
@@ -0,0 +1,16 @@
+import { graphql } from "react-apollo";
+import { getTaskQuery } from "../queries/queries";
+
+function TaskDetails(props) {
+  console.log(props);
+
+
+  return ( <
+    div id = "task-details" >
+    <
+    /
+    div >
+  );
+}
+
+export default TaskDetails;
